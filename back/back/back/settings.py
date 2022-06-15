18aa395f285b0b40d406cb215cd6e0e0e90671ff
U-Ar <yuma.arakawa82128awakara.amuy@gmail.com>
2022-06-15 21:52:46 +0900

    Update django setting for postgre

--- back/back/back/settings.py
+++ back/back/back/settings.py
@@ -79,6 +79,16 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+DATABASES = {
+  'default': {
+      'ENGINE': 'django.db.backends.postgresql',
+      'NAME': 'postgres',
+      'USER': 'postgres',
+      'PASSWORD': 'postgres', # これを追加
+      'HOST': 'db',
+      'PORT': 5432,
+    }
+}
 
 
 # Password validation
