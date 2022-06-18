bfd541501a80f20da2663002053c5e8bca98fcfb
sueCalystegia00 <52482280+susanbasket0410@users.noreply.github.com>
2022-06-18 14:02:28 +0900

    fix: APIを呼び出せない問題対処中...

--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -1,18 +1,37 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+	<div class="home">
+		<img alt="Vue logo" src="../assets/logo.png" />
+
+		<button @click="apiTest">APIテスト</button>
+		<div>
+			{{ result }}
+		</div>
+
+		<HelloWorld msg="Welcome to Your Vue.js App" />
+	</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
-export default {
-  name: "HomeView",
-  components: {
-    HelloWorld,
-  },
-};
+	// @ is an alias to /src
+	import HelloWorld from "@/components/HelloWorld.vue";
+	import axios from "axios";
+	export default {
+		name: "HomeView",
+		components: {
+			HelloWorld,
+		},
+		data() {
+			return {
+				result: null,
+			};
+		},
+		methods: {
+			async apiTest() {
+				await axios
+					.get("/api/user/some_id_from_google_or_something")
+					.then((response) => (this.result = response.data))
+					.catch((error) => console.log(error));
+			},
+		},
+	};
 </script>
