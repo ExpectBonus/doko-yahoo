9b80e25d9cd4dfbde67206134e8efc6498aced2c
sueCalystegia00 <52482280+susanbasket0410@users.noreply.github.com>
2022-06-18 15:28:22 +0900

    fix: APIの呼び出しにコンテナ名を指定すると成功

--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -1,37 +1,37 @@
 <template>
-	<div class="home">
-		<img alt="Vue logo" src="../assets/logo.png" />
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
 
-		<button @click="apiTest">APIテスト</button>
-		<div>
-			{{ result }}
-		</div>
+    <button @click="apiTest">APIテスト</button>
+    <div>
+      {{ result }}
+    </div>
 
-		<HelloWorld msg="Welcome to Your Vue.js App" />
-	</div>
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
 </template>
 
 <script>
-	// @ is an alias to /src
-	import HelloWorld from "@/components/HelloWorld.vue";
-	import axios from "axios";
-	export default {
-		name: "HomeView",
-		components: {
-			HelloWorld,
-		},
-		data() {
-			return {
-				result: null,
-			};
-		},
-		methods: {
-			async apiTest() {
-				await axios
-					.get("/api/user/some_id_from_google_or_something")
-					.then((response) => (this.result = response.data))
-					.catch((error) => console.log(error));
-			},
-		},
-	};
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+import axios from "axios";
+export default {
+  name: "HomeView",
+  components: {
+    HelloWorld,
+  },
+  data() {
+    return {
+      result: null,
+    };
+  },
+  methods: {
+    async apiTest() {
+      await axios
+        .get("/api/user/some_id_from_google_or_something")
+        .then((response) => (this.result = response.data))
+        .catch((error) => console.log(error));
+    },
+  },
+};
 </script>
