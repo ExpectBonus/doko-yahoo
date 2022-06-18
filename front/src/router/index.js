580ce858f475322da0ff350f077dee44a85d50bc
sueCalystegia00 <52482280+susanbasket0410@users.noreply.github.com>
2022-06-18 15:10:52 +0900

    clean: ページファイルの作成

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import TopView from "../views/TopView.vue";
+import ProfileView from "../views/ProfileView.vue";
+import MapView from "../views/MapView.vue";
 
 Vue.use(VueRouter);
 
@@ -11,13 +14,19 @@ const routes = [
 		component: HomeView,
 	},
 	{
-		path: "/about",
-		name: "about",
-		// route level code-splitting
-		// this generates a separate chunk (about.[hash].js) for this route
-		// which is lazy-loaded when the route is visited.
-		component: () =>
-			import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+		path: "/top",
+		name: "top",
+		component: TopView,
+	},
+	{
+		path: "/profile",
+		name: "profile",
+		component: ProfileView,
+	},
+	{
+		path: "/map",
+		name: "map",
+		component: MapView,
 	},
 ];
 
@@ -25,6 +34,18 @@ const router = new VueRouter({
 	mode: "history",
 	base: process.env.BASE_URL,
 	routes,
+});
+
+router.beforeEach(async (to, from, next) => {
+	//TODO: ログイン認証
+	/* if(!isAuth){
+		// 認証前のユーザは問答無用でトップに飛ばす
+		next({ name: 'top'})
+	}else{
+		next();
+	} */
+
+	next();
 });
 
 export default router;
