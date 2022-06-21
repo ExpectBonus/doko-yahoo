f4e50806da2c53d24815fe18a32270987bdcb431
U-Ar <yuma.arakawa82128awakara.amuy@gmail.com>
2022-06-21 21:04:11 +0900

    ヒートマップJSONの形式変更

--- back/test/heatmap_test.py
+++ back/test/heatmap_test.py
@@ -3,6 +3,14 @@ import pytest
 import math
 
 def test_get_heatmap_basic(app):
+    PREFECTURES = ["北海道","青森県","岩手県","宮城県","秋田県","山形県","福島県",
+    "茨城県","栃木県","群馬県","埼玉県","千葉県","東京都","神奈川県",
+    "新潟県","富山県","石川県","福井県","山梨県","長野県","岐阜県",
+    "静岡県","愛知県","三重県","滋賀県","京都府","大阪府","兵庫県",
+    "奈良県","和歌山県","鳥取県","島根県","岡山県","広島県","山口県",
+    "徳島県","香川県","愛媛県","高知県","福岡県","佐賀県","長崎県",
+    "熊本県","大分県","宮崎県","鹿児島県","沖縄県"]
+
     client = app.test_client()
 
     # POST
@@ -21,32 +29,32 @@ def test_get_heatmap_basic(app):
     })
 
     response = client.get("/api/heatmap/designer?hobbies=バイク&hobbies=温泉")
-    assert math.isclose(response.json["data"][0],1.0)
-    assert math.isclose(response.json["data"][1],2.0/3)
-    assert math.isclose(response.json["data"][2],1.0/3)
+    assert math.isclose(response.json["data"]["北海道"],1.0)
+    assert math.isclose(response.json["data"]["青森県"],2.0/3)
+    assert math.isclose(response.json["data"]["岩手県"],1.0/3)
     for i in range(3,47):
-        assert response.json["data"][i] == 0
+        assert response.json["data"][PREFECTURES[i]] == 0
 
     response = client.get("/api/heatmap/all?hobbies=バイク&hobbies=温泉")
-    assert math.isclose(response.json["data"][0],1.0)
-    assert math.isclose(response.json["data"][1],2.0/3)
-    assert math.isclose(response.json["data"][2],1.0/3)
+    assert math.isclose(response.json["data"]["北海道"],1.0)
+    assert math.isclose(response.json["data"]["青森県"],2.0/3)
+    assert math.isclose(response.json["data"]["岩手県"],1.0/3)
     for i in range(3,47):
-        assert response.json["data"][i] == 0
+        assert response.json["data"][PREFECTURES[i]] == 0
     
     response = client.get("/api/heatmap/designer")
-    assert math.isclose(response.json["data"][0],1.0)
-    assert math.isclose(response.json["data"][1],2.0/3)
-    assert math.isclose(response.json["data"][2],1.0/3)
+    assert math.isclose(response.json["data"]["北海道"],1.0)
+    assert math.isclose(response.json["data"]["青森県"],2.0/3)
+    assert math.isclose(response.json["data"]["岩手県"],1.0/3)
     for i in range(3,47):
-        assert response.json["data"][i] == 0
+        assert response.json["data"][PREFECTURES[i]] == 0
     
     response = client.get("/api/heatmap/all")
-    assert math.isclose(response.json["data"][0],1.0)
-    assert math.isclose(response.json["data"][1],2.0/3)
-    assert math.isclose(response.json["data"][2],1.0/3)
+    assert math.isclose(response.json["data"]["北海道"],1.0)
+    assert math.isclose(response.json["data"]["青森県"],2.0/3)
+    assert math.isclose(response.json["data"]["岩手県"],1.0/3)
     for i in range(3,47):
-        assert response.json["data"][i] == 0
+        assert response.json["data"][PREFECTURES[i]] == 0
 
     
     response = client.get("/api/heatmap/wrongname")
@@ -55,13 +63,13 @@ def test_get_heatmap_basic(app):
 
     response = client.get("/api/heatmap/engineer")
     for i in range(0,47):
-        assert response.json["data"][i] == 0
+        assert response.json["data"][PREFECTURES[i]] == 0
     response = client.get("/api/heatmap/business")
     for i in range(0,47):
-        assert response.json["data"][i] == 0
+        assert response.json["data"][PREFECTURES[i]] == 0
     response = client.get("/api/heatmap/all?hobbies=バイク&hobbies=おんせん")
     for i in range(0,47):
-        assert response.json["data"][i] == 0
+        assert response.json["data"][PREFECTURES[i]] == 0
 
 
 
@@ -83,22 +91,22 @@ def test_get_heatmap_basic(app):
 
 
     response = client.get("/api/heatmap/all?hobbies=バイク&hobbies=温泉")
-    assert math.isclose(response.json["data"][0],0.5)
-    assert math.isclose(response.json["data"][1],1.0/3)
-    assert math.isclose(response.json["data"][2],1.0/6)
-    assert math.isclose(response.json["data"][3],1.0)
+    assert math.isclose(response.json["data"]["北海道"],0.5)
+    assert math.isclose(response.json["data"]["青森県"],1.0/3)
+    assert math.isclose(response.json["data"]["岩手県"],1.0/6)
+    assert math.isclose(response.json["data"]["宮城県"],1.0)
     for i in range(4,47):
-        assert response.json["data"][i] == 0
+        assert response.json["data"][PREFECTURES[i]] == 0
     response = client.get("/api/heatmap/designer?hobbies=バイク&hobbies=温泉")
-    assert math.isclose(response.json["data"][0],1.0)
-    assert math.isclose(response.json["data"][1],2.0/3)
-    assert math.isclose(response.json["data"][2],1.0/3)
+    assert math.isclose(response.json["data"]["北海道"],1.0)
+    assert math.isclose(response.json["data"]["青森県"],2.0/3)
+    assert math.isclose(response.json["data"]["岩手県"],1.0/3)
     for i in range(3,47):
-        assert response.json["data"][i] == 0
+        assert response.json["data"][PREFECTURES[i]] == 0
     response = client.get("/api/heatmap/business?hobbies=バイク&hobbies=温泉")
-    assert math.isclose(response.json["data"][0],0)
-    assert math.isclose(response.json["data"][1],0)
-    assert math.isclose(response.json["data"][2],0)
-    assert math.isclose(response.json["data"][3],1.0)
+    assert math.isclose(response.json["data"]["北海道"],0)
+    assert math.isclose(response.json["data"]["青森県"],0)
+    assert math.isclose(response.json["data"]["岩手県"],0)
+    assert math.isclose(response.json["data"]["宮城県"],1.0)
     for i in range(4,47):
-        assert response.json["data"][i] == 0
+        assert response.json["data"][PREFECTURES[i]] == 0
