2899c67d7fe2bd72253c1118b71f037ad7ff968f
sueCalystegia00 <52482280+susanbasket0410@users.noreply.github.com>
2022-06-21 14:57:02 +0900

    add: 下部ドロワーの配置

--- front/src/views/MapView.vue
+++ front/src/views/MapView.vue
@@ -7,6 +7,9 @@
 		<div class="contents">
 			<Map :populationParameters="selectedHobbies" />
 		</div>
+		<div class="drawer-header">
+			<p>みんなはどこで働くつもりだろう？</p>
+		</div>
 	</div>
 </template>
 
@@ -30,16 +33,20 @@
 <style scoped>
 	#map-view {
 		width: 100%;
+		height: 100%;
 		display: flex;
 		flex-direction: column;
 		align-items: center;
 	}
 	header {
 		width: 100%;
+		position: fixed;
+		top: 0;
 		display: flex;
 		flex-direction: column;
 		align-items: center;
 		padding: 10px;
+		background-color: #ffffff;
 		box-shadow: 0px 4px 4px #cdcdcd;
 	}
 	h1 {
@@ -53,6 +60,31 @@
 	}
 	.contents {
 		width: 100%;
-		height: calc(70vh - 170px);
+		height: 100%;
+	}
+
+	.drawer-header {
+		width: 100%;
+		position: fixed;
+		bottom: 0;
+		height: 80px;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		font-weight: bold;
+		background-color: #d9d9d9;
+		border: 1px solid #d9d9d9;
+		border-radius: 20px 20px 0px 0px;
+	}
+	.drawer-header::before {
+		content: "";
+		display: block;
+		width: 20%;
+		border: 2px solid #878787;
+		border-radius: 2px;
+		position: absolute;
+		top: 5px;
+		left: 40%;
 	}
 </style>
