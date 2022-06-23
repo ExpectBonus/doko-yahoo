2deb0db546d2fed84281ecc9abb5c776d419c165
sho-yoshikawa <syoshika@student.42tokyo.jp>
2022-06-23 23:27:43 +0900

    create PrefComment component

---
+++ front/src/components/PrefComment.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="comment"></div>
+</template>
+<script>
+export default {
+  name: "PrefComment",
+  props: {
+    userComment: {
+      type: "object",
+      default: function () {
+        return {
+          comment: "仮のコメント",
+          born_pref: 12, //←テスト用
+          job: "エンジニア",
+        };
+      },
+    },
+  },
+};
+</script>
+<style scoped></style>
