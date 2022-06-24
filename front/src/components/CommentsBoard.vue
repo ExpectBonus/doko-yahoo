efe98667bcae7a970bc9ad3babcef3d36f5644ff
sueCalystegia00 <52482280+susanbasket0410@users.noreply.github.com>
2022-06-24 23:33:10 +0900

    add: コメント入力フォームを追加

--- front/src/components/CommentsBoard.vue
+++ front/src/components/CommentsBoard.vue
@@ -4,7 +4,24 @@
 			<p>{{ prefectureName || "みんなはどこで働くつもりだろう？" }}</p>
 		</div>
 		<div class="comments"></div>
-		<form></form>
+
+		<div class="message-setter">
+			<div class="text-input-container">
+				<textarea v-model="inputText"></textarea>
+			</div>
+
+			<button
+				class="send-button"
+				:disabled="!inputText"
+				@click="postNewComment()"
+			>
+				<svg xmlns="http://www.w3.org/2000/svg" height="50" width="50">
+					<path
+						d="M5 41.05V6.9L45.35 24ZM9.05 34.8 34.8 24 9.05 13.1V20.4L21.65 24L9.05 27.55ZM9.05 34.8V24V13.1V20.4V27.55Z"
+					/>
+				</svg>
+			</button>
+		</div>
 	</div>
 </template>
 
@@ -19,6 +36,11 @@
 				type: Array || Object,
 				default: () => {},
 			},
+		},
+		data() {
+			return {
+				inputText: "",
+			};
 		},
 	};
 </script>
@@ -56,5 +78,64 @@
 	.comments {
 		width: 100%;
 		height: calc(85% - 80px);
+	}
+
+	.message-setter {
+		width: 100%;
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		justify-content: space-between;
+		column-gap: 10px;
+		padding: 20px;
+	}
+	.text-input-container {
+		position: relative;
+		width: calc(100% - 50px);
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		background-color: #ffffff;
+		border: 3px solid #008277;
+		box-sizing: border-box;
+		border-radius: 10px;
+	}
+	.text-input-container:before {
+		/* 左ふきだしの左三角を描画 */
+		content: "";
+		position: absolute;
+		top: 15px;
+		left: -27px; /* 下記のborder値の合計 */
+		border: 12px solid transparent;
+		border-right: 15px solid #008277;
+	}
+	.text-input-container textarea {
+		width: 100%;
+		resize: none;
+		margin: 5px 10px;
+		border: none;
+		text-align: left;
+		font-weight: bold;
+		overflow-y: auto;
+	}
+	.text-input-container textarea:focus {
+		outline: none;
+	}
+	.send-button {
+		width: 50px;
+		height: 50px;
+		background: transparent;
+		border: none;
+		padding: 0;
+	}
+	.send-button:active {
+		position: relative;
+		top: 5px;
+	}
+	.send-button svg {
+		fill: #008277;
+	}
+	.send-button:disabled svg {
+		fill-opacity: 0.4;
 	}
 </style>
