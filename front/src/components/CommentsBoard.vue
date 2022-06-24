d355e7c1437a56be9048e1d48ab826e39e11495d
sueCalystegia00 <52482280+susanbasket0410@users.noreply.github.com>
2022-06-24 13:55:09 +0900

    update: スクロールでいい感じに切り替わるようにした👍

---
+++ front/src/components/CommentsBoard.vue
@@ -0,0 +1,56 @@
+<template>
+	<div id="comments-board">
+		<div class="drawer-header">
+			<p>{{ prefectureName || "みんなはどこで働くつもりだろう？" }}</p>
+		</div>
+		<div class="comments"></div>
+		<form></form>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: "CommentsBoard",
+		props: {
+			prefectureName: {
+				type: String,
+			},
+			comments: {
+				type: Array || Object,
+				default: () => {},
+			},
+		},
+	};
+</script>
+<style scoped>
+	#comments-board {
+		position: relative;
+		width: 100%;
+		height: 100vh;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		background-color: #d9d9d9;
+		border: 1px solid #d9d9d9;
+		border-radius: 20px 20px 0px 0px;
+	}
+	#comments-board::before {
+		content: "";
+		display: block;
+		width: 20%;
+		border: 2px solid #878787;
+		border-radius: 2px;
+		position: absolute;
+		top: 5px;
+		left: 40%;
+	}
+	.drawer-header {
+		width: 100%;
+		height: 80px;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		font-weight: bold;
+	}
+</style>
