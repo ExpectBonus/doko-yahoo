fb667f250f42360cf29bf1314d1c3cb5ac1fcf0d
sho-yoshikawa <syoshika@student.42tokyo.jp>
2022-06-25 21:24:50 +0900

    fix

--- front/src/components/PrefComment.vue
+++ front/src/components/PrefComment.vue
@@ -2,86 +2,61 @@
   <div class="comment">
     <div class="comment-container-left">
       <span class="comment-text">
-        {{userComment.comment}}
+        {{ userComment.comment }}
       </span>
       <div class="comment-userinfo">
         <div class="comment-job">
-          <span class="comment-job-text">{{userComment.job}}</span>
+          <span class="comment-job-text">{{ userComment.job }}</span>
         </div>
         <div class="comment-born">
-          <span class="comment-born-text">{{userComment.born_pref}}</span>
+          <span class="comment-born-text">{{ userComment.born_pref }}</span>
         </div>
       </div>
-      <div class="hukidasi-left"></div>
     </div>
-    <!-- ex -->
-    <!-- <div class="comment-container-left">
-      <span class="comment-text">
-        englishはこんな感じ。123はこんな感じ。あああああああああああああああああああああああ
-      </span>
-      <div class="comment-userinfo">
-        <div class="comment-job">
-          <span class="comment-job-text">ビジネス職</span>
-        </div>
-        <div class="comment-born">
-          <span class="comment-born-text">鹿児島県</span>
-        </div>
-      </div>
-      <div class="hukidasi-left"></div>
-    </div>
-  <div class="comment-container-right">
-    <span class="comment-text">
-      englishはこんな感じ。123はこんな感じ。あああああああああああああああああああああああ
-    </span>
-    <div class="comment-userinfo">
-      <div class="comment-job">
-        <span class="comment-job-text">ビジネス職</span>
-      </div>
-      <div class="comment-born">
-        <span class="comment-born-text">鹿児島県</span>
-      </div>
-    </div>
-    <div class="hukidasi-right"></div>
-  </div> -->
   </div>
 </template>
 <script>
 export default {
   name: "HelloWorld",
   props: {
-    // userComment: {
-    //   type: "",
-    //   default: function () {
-    //     return {
-    //       comment: "",
-    //       born_pref: 0, //←テスト用
-    //       job: "",
-    //     };
-    //   },
-    // },
+    userComment: {
+      type: "",
+      default: function () {
+        return {
+          comment: "",
+          born_pref: 0,
+          job: "",
+        };
+      },
+    },
   },
 };
 </script>
 <style scoped>
-
-.margin-box{
-  height: 50px;
-}
-
-.comment{
+.comment {
   text-align: center;
 }
-.comment-container-left{
-  position: relative;
+.comment-container-left {
   background: rgb(177, 174, 174);
   border-radius: 30px;
   color: black;
   padding: 30px 30px 10px 30px;
   max-width: 50%;
   margin: 0px auto 40px 0px;
-  }
+}
+
+.comment-container-left::after {
+  content: "";
+  position: absolute;
+  margin: 0;
+  border-top: 40px solid rgb(177, 174, 174);
+  border-left: 12px solid transparent;
+  border-right: 12px solid transparent;
+  transform: rotate(30deg);
+  left: 30px;
+}
+
 .comment-container-right {
-  position: relative;
   background: rgb(177, 174, 174);
   border-radius: 30px;
   color: black;
@@ -89,8 +64,20 @@ export default {
   padding: 30px 30px 10px 30px;
   margin: 0px 0px 40px auto;
 }
+
+.comment-container-right::after {
+  content: "";
+  position: absolute;
+  margin: 0;
+  border-top: 40px solid rgb(177, 174, 174);
+  border-left: 12px solid transparent;
+  border-right: 12px solid transparent;
+  transform: rotate(-30deg);
+  right: 30px;
+}
+
 .comment-text {
-  font-family: 'Courier New', Courier, monospace;
+  font-family: "Courier New", Courier, monospace;
   font-size: 25px;
 }
 
@@ -110,57 +97,37 @@ export default {
 }
 @media screen and (max-width: 500px) {
   .comment-container-left {
-      position: relative;
-      background: rgb(177, 174, 174);
-      border-radius: 30px;
-      color: black;
-      text-align: center;
-      padding: 30px 30px 10px 30px;
-      max-width: 90%;
-      margin-right: auto;
-    }
-    .comment-container-right {
-      position: relative;
-      background: rgb(177, 174, 174);
-      border-radius: 30px;
-      color: black;
-      text-align: left;
-      max-width: 90%;
-      padding: 30px 30px 10px 30px;
-      margin-left: auto;
-    }
-    .comment-text {
-      font-family: 'Courier New', Courier, monospace;
-      font-size: 15px;
-    }
-    .comment-job-text {
-      padding: 10px;
-      font-size: 13px;
-    }
+    position: relative;
+    background: rgb(177, 174, 174);
+    border-radius: 30px;
+    color: black;
+    text-align: center;
+    padding: 30px 30px 10px 30px;
+    max-width: 90%;
+    margin-right: auto;
+  }
+  .comment-container-right {
+    position: relative;
+    background: rgb(177, 174, 174);
+    border-radius: 30px;
+    color: black;
+    text-align: left;
+    max-width: 90%;
+    padding: 30px 30px 10px 30px;
+    margin-left: auto;
+  }
+  .comment-text {
+    font-family: "Courier New", Courier, monospace;
+    font-size: 15px;
+  }
+  .comment-job-text {
+    padding: 10px;
+    font-size: 13px;
+  }
 
-    .comment-born-text {
-      padding: 10px;
-      font-size: 13px;
-    }
-}
-
-.hukidasi-left{
-  position: absolute;
-  margin: 0;
-  border-top: 40px solid rgb(177, 174, 174);
-  border-left: 12px solid transparent;
-  border-right: 12px solid transparent;
-  transform: rotate(30deg);
-  left: 30px;
-}
-
-.hukidasi-right {
-  position: absolute;
-  margin: 0;
-  border-top: 40px solid rgb(177, 174, 174);
-  border-left: 12px solid transparent;
-  border-right: 12px solid transparent;
-  transform: rotate(-30deg);
-  right: 30px;
+  .comment-born-text {
+    padding: 10px;
+    font-size: 13px;
+  }
 }
 </style>
