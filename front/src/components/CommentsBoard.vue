b766d5993e11002f71cea879fae8810a0f756e77
sueCalystegia00 <52482280+susanbasket0410@users.noreply.github.com>
2022-06-26 00:57:06 +0900

    微調整

--- front/src/components/CommentsBoard.vue
+++ front/src/components/CommentsBoard.vue
@@ -83,11 +83,12 @@
 	}
 	.drawer-header {
 		width: 100%;
-		height: 80px;
+		height: 15%;
 		display: flex;
 		flex-direction: column;
 		align-items: center;
 		justify-content: center;
+		font-size: 24px;
 		font-weight: bold;
 	}
 	.comments {
